253df4d454e07e346ce6641fca6c75963f73365a
yerassyl <y.bolatov1@gmail.com>
2022-10-05 13:32:42 +0600

    configs are properly initialized

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	database "github.com/yerassylbolatov/crudmux/db"
 	"github.com/yerassylbolatov/crudmux/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	db, err := database.DbStart()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error at initConfig occured %s", err.Error())
+	}
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env parameters: %s", err.Error())
+	}
+	db, err := database.DbStart(&database.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetInt("db.port"),
+		User:     viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DbName:   viper.GetString("db.dbname"),
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,5 +33,11 @@ func main() {
 	handlers.Db = db
 	routes := handlers.InitRoutes()
 	fmt.Println("server starts at :8080")
-	log.Fatal(http.ListenAndServe(":8080", routes))
+	log.Fatal(http.ListenAndServe(viper.GetString("port"), routes))
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- db/db.go
+++ db/db.go
@@ -5,30 +5,35 @@ import (
 	"fmt"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
-	"os"
 )
 
-const (
-	Host    = "localhost"
-	Port    = "5432"
-	User    = "postgres"
-	DabName = "postgres"
-)
+//const (
+//	Host    = "localhost"
+//	User    = "postgres"
+//	DabName = "postgres"
+//)
 
-func DbStart() (*sql.DB, error) {
+type Config struct {
+	Host     string
+	Port     int
+	User     string
+	DbName   string
+	Password string
+}
+
+func DbStart(config *Config) (*sql.DB, error) {
 	if err := godotenv.Load(); err != nil {
-		fmt.Printf("error when loading environment variables %s\n", err.Error())
 		return nil, err
 	}
-	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", Host, Port, User, os.Getenv("DB_PASSWORD"), DabName)
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		config.Host, config.Port, config.User, config.Password, config.DbName,
+	)
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
-		fmt.Printf("error when openinig sql %s\n", err.Error())
 		return nil, err
 	}
 	err = db.Ping()
 	if err != nil {
-		fmt.Printf("error when pinging sql %s\n", err.Error())
 		return nil, err
 	}
 	return db, nil
